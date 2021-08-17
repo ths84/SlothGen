2c5c57a54a6f49efe59316ab16f09a5d914a7fa6
ths84 <t.shmdt@gmail.com>
2021-08-17 14:09:21 +0200

    test

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='SlothGen',
-    version='0.1',
-    author='ths84',
-    author_email='t.shmdt@gmail.com',
-    packages=find_packages(),
-)
